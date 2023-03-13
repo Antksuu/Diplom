4cab57fe60d79c537f13cdedfb24f429701f3cfb
Antksuu <antksyha@icloud.com>
2023-03-13 17:32:45 +0300

    Initial commit

---
+++ Diplom/src/main/java/com/example/Diplom/Model/Role.java
@@ -0,0 +1,16 @@
+package com.example.Diplom.Model;
+
+public enum Role {
+    USER("User"),
+    ADMIN("Admin");
+
+    private final String value;
+
+    private Role(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
